d5d616f9e61d8d09e190fe13ec329aa8a7ab352a
ThanhhDaoo <tranthanhdao82@gmail.com>
2025-10-05 12:26:35 +0700

    xu li AndroidMainifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Thêm CardView cho giao diện
+    implementation("androidx.cardview:cardview:1.0.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
